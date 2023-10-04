d6eaafdae0db3cbea0850e4c50e32586052435ad
Martin House <torchester@gmail.com>
2023-10-04 10:10:52 +0500

    fix hw2

--- Test/seminars/second/hw/VehicleTest.java
+++ Test/seminars/second/hw/VehicleTest.java
@@ -9,36 +9,36 @@ class VehicleTest {
     // Проверить, что экземпляр объекта Car также является экземпляром
     // транспортного средства (используя оператор instanceof)
     @Test
-    public void testCarIsInstanceOfVehicle() {
+    public void carIsInstanceOfVehicle() {
         Car car = new Car("Tesla", "Model X", 2017);
         assertTrue(car instanceof Vehicle);
     }
 
     // Проверить, что объект Car создается с 4-мя колесами
     @Test
-    public void testCarIsFourWheels() {
+    public void carHasFourWheels() {
         Car car = new Car("Tesla", "Model X", 2017);
         assertTrue(car.getNumWheels() == 4);
     }
 
     // Проверить, что объект Motorcycle создается с 2-мя колесами
     @Test
-    public void testMotoIsTwoWheels() {
+    public void motoHasTwoWheels() {
         Motorcycle motorcycle = new Motorcycle("Aprilia", "Pegaso", 2008);
         assertTrue(motorcycle.getNumWheels() == 2);
     }
 
     // Проверить, что объект Car развивает скорость 60 в режиме тестового вождения (используя метод testDrive())
     @Test
-    public void testCarIsSpeedTest() {
-        Car car = new Car("Toyota", "Corolla", 2015);
+    public void carSpeedTest() {
+        Car car = new Car("Tesla", "Model X", 2017);
         car.testDrive();
         assertTrue(car.getSpeed() == 60);
     }
 
     // Проверить, что объект Motorcycle развивает скорость 75 в режиме тестового вождения (используя метод testDrive())
     @Test
-    public void testMotoIsSpeedTest() {
+    public void motoSpeedTest() {
         Motorcycle motorcycle = new Motorcycle("Aprilia", "Pegaso", 2008);
         motorcycle.testDrive();
         assertTrue(motorcycle.getSpeed() == 75);
@@ -47,7 +47,7 @@ class VehicleTest {
     // Проверить, что в режиме парковки (сначала testDrive, потом park, т.е. эмуляция движения транспорта)
     // машина останавливается (speed = 0)
     @Test
-    public void testCarIsStopAfterParking() {
+    public void carStoppedAfterParking() {
         Car car = new Car("Tesla", "Model X", 2017);
         car.testDrive();
         car.park();
@@ -57,7 +57,7 @@ class VehicleTest {
     // Проверить, что в режиме парковки (сначала testDrive, потом park, т.е. эмуляция движения транспорта)
     // мотоцикл останавливается (speed = 0)
     @Test
-    public void testMotoIsStopAfterParking() {
+    public void motoStoppedAfterParking() {
         Motorcycle motorcycle = new Motorcycle("Aprilia", "Pegaso", 2008);
         motorcycle.testDrive();
         motorcycle.park();
